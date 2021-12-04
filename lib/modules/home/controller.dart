c576d864d83286acb1e22e1cd1d1c8b9bc7ef1f0
negu63 <dnjsdn2019@gmail.com>
2021-12-04 23:00:13 +0900

    feat: 넘버 컨트롤러 콜백 함수 추가 및 디바운스 적용

--- lib/modules/home/controller.dart
+++ lib/modules/home/controller.dart
@@ -2,5 +2,25 @@ import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class NumberController extends GetxController {
+  RxInt age = 0.obs;
   RxInt lifeSpan = 30000.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    debounce(
+      age,
+      caculateLifeSpan,
+      time: const Duration(milliseconds: 500),
+    );
+  }
+
+  ageChanged(int value) {
+    age.value = value;
+  }
+
+  caculateLifeSpan(int age) {
+    lifeSpan.value = 30000 - age * 365;
+  }
 }
