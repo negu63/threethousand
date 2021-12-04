3f9b371f3f452d3e05a6f5c4a9f162953247db11
negu63 <dnjsdn2019@gmail.com>
2021-12-04 23:11:02 +0900

    feat: 나이 선택 위젯 추가

---
+++ lib/modules/home/local_widgets/age_picker.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:threethousand/modules/home/controller.dart';
+
+class AgePicker extends StatelessWidget {
+  const AgePicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final numberController = Get.find<NumberController>();
+    return Obx(
+      () => NumberPicker(
+        minValue: 0,
+        maxValue: 969,
+        value: numberController.age.value,
+        onChanged: numberController.ageChanged,
+      ),
+    );
+  }
+}
