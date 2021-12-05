84198cb7a20170f8b26d6e309a601f5d1aee3f5f
negu63 <dnjsdn2019@gmail.com>
2021-12-05 22:56:13 +0900

    feat: 홈 페이지 스크롤 설정 변경 및 위젯 추가

--- lib/modules/home/page.dart
+++ lib/modules/home/page.dart
@@ -1,22 +1,57 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sliding_number/sliding_number.dart';
-import 'package:threethousand/modules/home/controller.dart';
+import 'package:threethousand/modules/home/local_widgets/label_text.dart';
+import 'package:threethousand/modules/home/local_widgets/age_picker.dart';
+import 'package:threethousand/modules/home/local_widgets/life_span.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final NumberController numberController = Get.find();
     return Scaffold(
-      body: Center(
-        child: Obx(
-          () => SlidingNumber(
-            number: numberController.lifeSpan.value,
-            style: Theme.of(context).textTheme.headline3!,
-            duration: const Duration(milliseconds: 2000),
-            curve: Curves.easeOut,
+      body: SafeArea(
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(
+            scrollbars: false,
+            dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+            },
+            physics: const BouncingScrollPhysics(),
+          ),
+          child: Center(
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    LabelText(
+                      text: 'aboutText'.tr,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      padding: const EdgeInsets.only(
+                        left: 50,
+                      ),
+                    ),
+                    const LifeSpan(),
+                    LabelText(
+                      text: 'daysText'.tr,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      padding: const EdgeInsets.only(
+                        right: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    LabelText(text: 'ageText'.tr),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: AgePicker(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
