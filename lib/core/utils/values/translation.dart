cd58872d9ea4bd6cb83c8390cfee6c7b2c2c7624
negu63 <dnjsdn2019@gmail.com>
2021-12-05 22:17:12 +0900

    feat: 번역 텍스트 추가

--- lib/core/utils/values/translation.dart
+++ lib/core/utils/values/translation.dart
@@ -4,14 +4,16 @@ class Messages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          'appTitle': '',
-          'aboutText': 'about',
+          'appTitle': 'About 30000 days',
+          'aboutText': 'About',
           'daysText': 'days',
+          'ageText': 'Age',
         },
         'ko_KR': {
-          'appTitle': '',
-          'aboutTitle': '약',
+          'appTitle': '약 30000일',
+          'aboutText': '약',
           'daysText': '일',
+          'ageText': '나이',
         },
       };
 }
