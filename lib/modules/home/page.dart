0e1722dc210a225a2e5fc69568fc6565bc8a419f
negu63 <dnjsdn2019@gmail.com>
2021-12-05 23:40:13 +0900

    feat: 홈 페이지 구성 변경

--- lib/modules/home/page.dart
+++ lib/modules/home/page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:threethousand/modules/home/local_widgets/label_text.dart';
-import 'package:threethousand/modules/home/local_widgets/age_picker.dart';
+import 'package:threethousand/modules/home/local_widgets/birthday_picker.dart';
 import 'package:threethousand/modules/home/local_widgets/life_span.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,27 +26,11 @@ class HomePage extends StatelessWidget {
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    LabelText(
-                      text: 'aboutText'.tr,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      padding: const EdgeInsets.only(
-                        left: 50,
-                      ),
-                    ),
+                    const AboutText(),
                     const LifeSpan(),
-                    LabelText(
-                      text: 'daysText'.tr,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      padding: const EdgeInsets.only(
-                        right: 50,
-                      ),
-                    ),
+                    const DaysText(),
                     const SizedBox(height: 50),
-                    LabelText(text: 'ageText'.tr),
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: AgePicker(),
-                    ),
+                    const BirthdayPicker(),
                   ],
                 ),
               ],
