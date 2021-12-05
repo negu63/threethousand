b7150f69ace4576eed85cdb6a92302efd1593c9e
negu63 <dnjsdn2019@gmail.com>
2021-12-05 22:25:06 +0900

    refactor: 라이센스 버튼 이름 변경

---
+++ lib/modules/home/local_widgets/oss_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OssButton extends StatelessWidget {
+  const OssButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(
+        Icons.info_outline,
+      ),
+      onPressed: () {
+        Get.toNamed('/oss');
+      },
+    );
+  }
+}
