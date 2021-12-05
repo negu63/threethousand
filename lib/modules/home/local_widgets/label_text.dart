d83194811cb342648d943bfa606af34f6d27f896
negu63 <dnjsdn2019@gmail.com>
2021-12-05 23:40:01 +0900

    refactor: label 텍스트 리팩토링

--- lib/modules/home/local_widgets/label_text.dart
+++ lib/modules/home/local_widgets/label_text.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
+
+class AboutText extends StatelessWidget {
+  const AboutText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LabelText(
+      text: 'aboutText'.tr,
+      mainAxisAlignment: MainAxisAlignment.start,
+      padding: const EdgeInsets.only(
+        left: 50,
+      ),
+    );
+  }
+}
+
+class DaysText extends StatelessWidget {
+  const DaysText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LabelText(
+      text: 'daysText'.tr,
+      mainAxisAlignment: MainAxisAlignment.end,
+      padding: const EdgeInsets.only(
+        right: 50,
+      ),
+    );
+  }
+}
 
 class LabelText extends StatelessWidget {
   const LabelText({
     Key? key,
     required this.text,
-    this.padding,
     this.mainAxisAlignment,
+    this.padding,
+    this.style,
   }) : super(key: key);
 
   final MainAxisAlignment? mainAxisAlignment;
   final EdgeInsets? padding;
+  final TextStyle? style;
   final String text;
 
   @override
@@ -21,7 +54,7 @@ class LabelText extends StatelessWidget {
           padding: padding ?? EdgeInsets.zero,
           child: Text(
             text,
-            style: Theme.of(context).textTheme.headline5!,
+            style: style ?? Theme.of(context).textTheme.headline5!,
           ),
         ),
       ],
