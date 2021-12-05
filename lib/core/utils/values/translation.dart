1ee9b114662991fc04141682d83ba1c8414b01c9
negu63 <dnjsdn2019@gmail.com>
2021-12-05 23:44:47 +0900

    chore: 번역 텍스트 앱 이름 변경

--- lib/core/utils/values/translation.dart
+++ lib/core/utils/values/translation.dart
@@ -4,7 +4,7 @@ class Messages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          'appTitle': 'About 30000 days',
+          'appTitle': '30000 days',
           'aboutText': 'About',
           'daysText': 'days',
           'yearText': 'Year',
@@ -12,7 +12,7 @@ class Messages extends Translations {
           'dayText': 'Day',
         },
         'ko_KR': {
-          'appTitle': '약 30000일',
+          'appTitle': '30000일',
           'aboutText': '약',
           'daysText': '일',
           'yearText': '년',
