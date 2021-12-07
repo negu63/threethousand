e648fff8963ccdd0aa99a5956f176ee30396e87f
negu63 <dnjsdn2019@gmail.com>
2021-12-07 22:03:44 +0900

    feat: 페이지 클래스 공지 페이지 추가

--- lib/modules/routes/pages.dart
+++ lib/modules/routes/pages.dart
@@ -1,11 +1,16 @@
 import 'package:get/get.dart';
 import 'package:threethousand/modules/home/page.dart';
 import 'package:threethousand/modules/oss/page.dart';
+import 'package:threethousand/modules/warning/page.dart';
 
 import 'routes.dart';
 
 class AppPages {
   static final pages = [
+    GetPage(
+      name: Routes.notice,
+      page: () => const NoticePage(),
+    ),
     GetPage(
       name: Routes.home,
       page: () => const HomePage(),
